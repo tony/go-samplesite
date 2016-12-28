be4ba3ed4408ab910209d92403eb2365b6d5f0f4
Tony Narlock <tony@git-pull.com>
2016-12-27 23:11:41 -0600

    switch to ging

--- main.go
+++ main.go
@@ -1,36 +1,20 @@
 package main
 
-// ./main.go
-import "github.com/kataras/iris"
+import "net/http"
+
+import "gopkg.in/gin-gonic/gin.v1"
 
 func main() {
-	iris.Get("/hi", hi)
-	// http://localhost:5700/api/user/42
-	// Method: "GET"
-	iris.Get("/api/user/:id", func(ctx *iris.Context) {
-
-		// take the :id from the path, parse to integer
-		// and set it to the new userID local variable.
-		userID, _ := ctx.ParamInt("id")
-
-		// userRepo, imaginary database service <- your only job.
-		user := struct {
-			ID   int
-			Name string
-		}{ID: userID, Name: "tony"}
-
-		// send back a response to the client,
-		// .JSON: content type as application/json; charset="utf-8"
-		// iris.StatusOK: with 200 http status code.
-		//
-		// send user as it is or make use of any json valid golang type,
-		// like the iris.Map{"username" : user.Username}.
-		ctx.JSON(iris.StatusOK, user)
-
-	})
-	iris.Listen("localhost:8080")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+	router.GET("/", hi)
+	router.GET("/index", hi)
+	router.Run("localhost:8080")
 }
 
-func hi(ctx *iris.Context) {
-	ctx.Render("hi.html", struct{ Name string }{Name: "iris"})
+func hi(c *gin.Context) {
+	c.HTML(http.StatusOK, "hi.html", gin.H{
+		"title": "Main website",
+	})
 }
